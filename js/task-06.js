cfc5e42e3eb2c358c09a0361fbe1b5cb0ca35ff8
Albina Shmatko <albina.shmatko@gmail.com>
2022-10-31 13:36:04 +0200

    Updates the files

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,13 @@ const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener('blur', checkValidation);
 
 function checkValidation(event) {
-    if (event.target.value.length == inputEl.getAttribute("data-length")) {
-      inputEl.classList.add("valid");
-    } else inputEl.classList.add("invalid");
-}  
+  if (event.target.value.length == inputEl.getAttribute("data-length")) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid");
+  }
+};
 
 console.log(inputEl);
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,7 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const body = document.body;
@@ -7,6 +9,6 @@ const colorEl = document.querySelector(".color");
 const btnChangeColorEl = document.querySelector(".change-color");
 
 btnChangeColorEl.addEventListener("click", () => {
-  body.style.backgroundColor = getRandomHexColor();
   colorEl.innerHTML = getRandomHexColor();
+  body.style.backgroundColor = colorEl.innerHTML;
 });
